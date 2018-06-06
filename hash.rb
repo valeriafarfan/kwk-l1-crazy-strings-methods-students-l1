046d79c6a64c5c970295f12f722c1aee72f3bac2
valeria farfan <valeria073104@gmail.com>
2018-06-06 17:37:09 +0000

    automatically backed up by learn

--- hash.rb
+++ hash.rb
@@ -1,18 +1,48 @@
-cart_item_prices = [48:23, 6:97, 26:95, 129:99]
+# cart_item_prices = [48:23, 6:97, 26:95, 129:99]
 
-count = 1 
+# count = 1 
 
-cart_item_prices.each do |price| #.each do | | iterates meaning goes through each element in #the array in order. in this case I'm calling each element "price",but you can call it #anything 
-puts "item 
-
-
+# cart_item_prices.each do |price| #.each do | | iterates meaning goes through each element in #the array in order. in this case I'm calling each element "price",but you can call it #anything 
+# puts item 
 
 
 
 
-cart_item_prices = [48.23,6.97,26.95,129.99]
 
-tax = [] #array that will hold tax price of each item
 
-count = 1
+# cart_item_prices = [48.23,6.97,26.95,129.99]
+
+# tax = [] #array that will hold tax price of each item
+
+# count = 1
+# cart_item_prices.each do |price|
+#   tax_of_item = price + 0.15
+#   tax.push(tax_of_item_) #puts the tax of each item into the array called tax
+#   puts "Item #{count} has a tax of #{tax_of_item}"
+  
+#   count += 15
+  
+# end
+
+
+
+
+
+
+cart_item_prices = [48.25, 6.97, 26.95, 129.99]
+expensive_things = [] # makes an array called expensive_things that will hold items more                                         #expensive than $50
+
 cart_item_prices.each do |price|
+  if price >=$50
+    expensive_things << price #inputs price that is more than 50$ into the array                                                      expensive_things
+  end
+end
+
+puts expensive_things # print out an array of items more than $50
+
+
+# wall_clock = 48.23
+# pop_socket = 6.97
+# spiralizer = 26.95
+# echo_plus = 129.99
+  
